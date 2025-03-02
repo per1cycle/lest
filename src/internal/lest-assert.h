7ae660b2cb66c158d7aa84ff4510073d9c69e929
Huan Zhou <me@per1cycle.org>
2025-03-02 17:58:32 +0800

    Update assert.

--- src/internal/lest-assert.h
+++ src/internal/lest-assert.h
@@ -6,57 +6,14 @@
 #include <sstream>
 
 #include "internal/lest-define.h"
+
+#define EXPECT_EQ(expected, actual) lest::Assert::AreEqual(expected, actual)
+
 namespace lest {
-
-class AssertionError : public std::runtime_error {
-public:
-    explicit AssertionError(const std::string& message) : std::runtime_error(message) {}
-};
-
-class Assert {
-public:
-    static void AreEqual(int expected, int actual, const std::string& message = "") {
-        if (expected != actual) {
-            std::ostringstream error;
-            error << "Expected: " << expected << ", but got: " << actual;
-            if (!message.empty()) {
-                error << " - " << message;
-            }
-            throw AssertionError(error.str());
-        }
-    }
-
-    static void AreEqual(const std::string& expected, const std::string& actual, const std::string& message = "") {
-        if (expected != actual) {
-            std::ostringstream error;
-            error << "Expected string: \"" << expected << "\", but got: \"" << actual << "\"";
-            if (!message.empty()) {
-                error << " - " << message;
-            }
-            throw AssertionError(error.str());
-        }
-    }
-
-    static void IsTrue(bool condition, const std::string& message = "") {
-        if (!condition) {
-            std::string error = "Expected true but got false";
-            if (!message.empty()) {
-                error += " - " + message;
-            }
-            throw AssertionError(error);
-        }
-    }
-
-    static void IsFalse(bool condition, const std::string& message = "") {
-        if (condition) {
-            std::string error = "Expected false but got true";
-            if (!message.empty()) {
-                error += " - " + message;
-            }
-            throw AssertionError(error);
-        }
-    }
-};
+/**
+ * consider a better way to implement assert.
+ */
+template <typename t1, typename t2>
 
 } // namespace lest
 
