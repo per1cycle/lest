e49a7b032f7d90d7e11f9f06c63435aa3de5075c
Huan Zhou <me@per1cycle.org>
2025-03-17 21:20:56 +0800

    Upload microtest.

--- src/lest-testing.cc
+++ src/lest-testing.cc
@@ -12,7 +12,7 @@ Test::Test()
 Test::Test(const std::string& test_group, const std::string& test_name)
     : test_group_(test_group), test_name_(test_name)
 {
-
+    LOG_INFO << "Running Test: " << test_group_ << "_" << test_name;
 }
 
 TestInfo::TestInfo()
