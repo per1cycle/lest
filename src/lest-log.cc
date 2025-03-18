c87b802c12d0c5b8ba3a48aa60fa121c0e630df5
Huan Zhou <me@per1cycle.org>
2025-03-18 13:34:13 +0800

    Fixing gcc.

---
+++ src/lest-log.cc
@@ -0,0 +1,7 @@
+#include "internal/lest-define.h"
+
+const char* LogImpl::level_name[Level::NUM] = {
+    "\033[1;32mINFO\033[1;37m",
+    "\033[1;33mWARNING\033[1;37m",
+    "\033[1;31mERROR\033[1;37m",
+};
