9104f3cefc17a95f2689d9c70582a528a048e6b1
Huan Zhou <me@per1cycle.org>
2025-03-18 14:23:41 +0800

    add playground.

--- playground/playground.cc
+++ playground/playground.cc
@@ -1,8 +1,23 @@
 #include <lest.h>
+#include <iostream>
+#define CLASS_NAME(x) #x
+class Foo
+{
+public:
+    Foo() {}
+
+public:
+    int foo()
+    {
+        std::cout << typeid(this).name() << " " << __func__ << std::endl;
+        return 0;
+    }
+};
 
 int main(int argc, char const *argv[])
 {
     std::cout << LEST_VERSION << std::endl;    
-     
+    Foo f;
+    f.foo(); 
     return 0;
 }
