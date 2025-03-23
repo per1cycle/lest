434850953a9f0ea170e600a8c218785c7603353f
Huan Zhou <me@per1cycle.org>
2025-03-23 14:03:22 +0800

    Add more compare type.

--- src/lest.h
+++ src/lest.h
@@ -55,7 +55,22 @@
 #define LT(expression1, expression2) \
     LEST_EXPECT(lest::Compare::LT, expression1, expression2)
 
-#define CMP(expression1, expression2, cmp_class) \
+#define CMP_EQ(expression1, expression2, cmp_class) \
+    LEST_EXPECT_CUSTOM_CMP(lest::Compare::CMP, expression1, expression2, cmp_class)
+
+#define CMP_NE(expression1, expression2, cmp_class) \
+    LEST_EXPECT_CUSTOM_CMP(lest::Compare::CMP, expression1, expression2, cmp_class)
+
+#define CMP_GT(expression1, expression2, cmp_class) \
+    LEST_EXPECT_CUSTOM_CMP(lest::Compare::CMP, expression1, expression2, cmp_class)
+
+#define CMP_GE(expression1, expression2, cmp_class) \
+    LEST_EXPECT_CUSTOM_CMP(lest::Compare::CMP, expression1, expression2, cmp_class)
+
+#define CMP_LT(expression1, expression2, cmp_class) \
+    LEST_EXPECT_CUSTOM_CMP(lest::Compare::CMP, expression1, expression2, cmp_class)
+
+#define CMP_LE(expression1, expression2, cmp_class) \
     LEST_EXPECT_CUSTOM_CMP(lest::Compare::CMP, expression1, expression2, cmp_class)
 
 #define AEQ(expression1, expression2) \
@@ -82,9 +97,23 @@
 #define ALT(expression1, expression2) \
     LEST_ASSERT(lest::Compare::LT, expression1, expression2)
 
-#define ACMP(expression1, expression2, cmp_class) \
-    LEST_ASSERT_CUSTOM_CMP(lest::Compare::CMP, expression1, expression2, cmp_class)
+#define ACMP_EQ(expression1, expression2, cmp_class) \
+    LEST_ASSERT_CUSTOM_CMP(lest::Compare::EQ, expression1, expression2, cmp_class)
 
+#define ACMP_NE(expression1, expression2, cmp_class) \
+    LEST_ASSERT_CUSTOM_CMP(lest::Compare::NE, expression1, expression2, cmp_class)
+
+#define ACMP_GT(expression1, expression2, cmp_class) \
+    LEST_ASSERT_CUSTOM_CMP(lest::Compare::GT, expression1, expression2, cmp_class)
+
+#define ACMP_GE(expression1, expression2, cmp_class) \
+    LEST_ASSERT_CUSTOM_CMP(lest::Compare::GE, expression1, expression2, cmp_class)
+
+#define ACMP_LT(expression1, expression2, cmp_class) \
+    LEST_ASSERT_CUSTOM_CMP(lest::Compare::LT, expression1, expression2, cmp_class)
+
+#define ACMP_LE(expression1, expression2, cmp_class) \
+    LEST_ASSERT_CUSTOM_CMP(lest::Compare::LE, expression1, expression2, cmp_class)
 
 static int RUN()
 {
